740b320219f15339b2bbe571ec39a7529bd3300d
twoDarkMessiah <twodarkmessiah@users.noreply.github.com>
2018-12-22 19:27:50 +0100

    implemented basic wrapper functions

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # py-pgpmail
-Simple wrapper to send pgp mails over smtp
+##### Simple wrapper to send pgp mails over smtp
+######How to Use
+
+
+1. Import PGP Keys (gpg --keyserver keyserver.cybernation.eu --recv KEY_ID )
+2. Send PGP Mail via Python
+```python
+if __name__ == "__main__":
+    send_smtp_pgp_mail("mail.riseup.net", "webmaster@riseup.net", 
+    "securePassword;)", "Webmaster<webmaster@cybernation.eu>",
+     "THIS TEXT WILL BE ENCRYPTED", server_port=465)
+
+```
---
+++ pgpmail.py
@@ -0,0 +1,70 @@
+import re
+import smtplib
+from email import encoders
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+
+import gnupg
+
+
+class MIMEApplicationPGPPayload(MIMEApplication):
+    def __init__(self, _data,
+                 _subtype='octet-stream; name="encrypted.asc"',
+                 _encoder=encoders.encode_noop,
+                 **_params):
+        _params["Content-Description"] = "OpenPGP encrypted message"
+        _params["Content-Disposition"] = 'inline; filename="encrypted.asc"'
+        MIMEApplication.__init__(self, _data=_data, _subtype=_subtype,
+                                 _encoder=_encoder, **_params)
+
+
+class MIMEApplicationPGPDescription(MIMEApplication):
+    def __init__(self, _data="Version: 1\n", _subtype='pgp-encrypted',
+                 _encoder=encoders.encode_noop,
+                 **_params):
+        _params["Content-Description"] = "PGP/MIME version identification"
+        MIMEApplication.__init__(self, _data=_data, _subtype=_subtype,
+                                 _encoder=_encoder, **_params)
+
+
+class MIMEMultipartPGP(MIMEMultipart):
+    def __init__(self, _data=None, _subtype='encrypted', boundary=None,
+                 **_params):
+        _params['protocol'] = "application/pgp-encrypted"
+        description = MIMEApplicationPGPDescription()
+        payload = MIMEApplicationPGPPayload(_data)
+        _subparts = [description, payload]
+        MIMEMultipart.__init__(self, _subtype=_subtype, boundary=boundary,
+                               _subparts=_subparts, **_params)
+
+
+def send_smtp_pgp_mail(server_address, username, password, recp, msg, sender=None, server_port=25):
+    ssl = None
+    smtp_server = None
+
+    if sender == None:
+        sender = username
+
+    gpg = gnupg.GPG()
+    gpg_msg = ""
+
+    if "<" in recp:
+        gpg_msg = str(gpg.encrypt(msg, re.findall("<\S{1,}>", recp)[0]))
+    else:
+        gpg_msg = str(gpg.encrypt(msg, recp))
+
+    if server_port == 465:
+        # Initialize SSL Connection
+        smtp_server = smtplib.SMTP_SSL(server_address, server_port)
+    else:
+        # Initialize Unsecure Connection
+        smtp_server = smtplib.SMTP(server_address, server_port)
+
+    smtp_server.login(username, password)
+
+    msg = MIMEMultipartPGP(gpg_msg)
+
+    msg['Subject'] = "Python PGP Test"
+    msg["From"] = sender
+    msg["To"] = recp
+    smtp_server.send_message(msg)
